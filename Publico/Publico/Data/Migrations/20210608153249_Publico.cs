920a7dc6ba4ea89dbf65f9037acb32ca5178fbee
Ayeager2 <annanyeager@gmail.com>
2021-06-08 15:24:09 -0400

    Publico fixish/ContosoUniversity
    fix it so publico has a starting point - ContoscoUniveristy used to understand local DB so I can work on sign in functionality of Publico

---
+++ Publico/Publico/Data/Migrations/20210608153249_Publico.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Publico.Data.Migrations
+{
+    public partial class Publico : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Publico/Publico/Hub/Chat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Publico
+{
+    public class Chat : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", Context.User.Identity.Name ?? "anonymous", message);
+        }
+    }
+}
