6ba4693f1be3beff39fd81e651bc0fa1e5a932ec
Ayeager2 <annanyeager@gmail.com>
2021-06-11 18:36:33 -0400

    Some Changes
    Lesson stuff

--- IntroductionToLinq/Cars/Program.cs
+++ IntroductionToLinq/Cars/Program.cs
@@ -9,35 +9,93 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            var cars = ProcessFile("fuel.csv");
+            var cars = ProcessCars("fuel.csv");
+            var manufacturers = ProcessManufacturers("manufacturers.csv");
 
-            var query = cars.OrderByDescending(c => c.Combined)
-                .Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
-                .OrderByDescending(c => c.Combined)
-                .ThenBy(c => c.Name);
-
-            var first = cars.OrderByDescending(c => c.Combined)              
-              .OrderByDescending(c => c.Combined)
-              .ThenBy(c => c.Name)
-              .First(c => c.Manufacturer == "BMW" && c.Year == 2016);
-
+            var query =
+                from car in cars
+                join manufacturer in manufacturers
+                    on car.Manufacturer equals manufacturer.Name
+                orderby car.Combined descending, car.Name ascending
+                select new
+                {
+                    manufacturer.Headquarters,
+                    car.Name,
+                    car.Combined
+                };
             foreach (var car in query.Take(10))
             {
-                Console.WriteLine($"{car.Name} : {car.Combined}");
+                Console.WriteLine($"{car.Headquarters} {car.Name} : {car.Combined}");
             }
+
+            //select new
+            //{
+            //    car.Manufacturer,
+            //    car.Name,
+            //    car.Combined
+            //};
+
+
+            //var result = cars.SelectMany(c => c.Name)
+            //    .OrderBy(c => c);
+
+            //foreach (var name in result)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            //var result = cars.Select(c => c.Name);
+
+            //var result = cars.Select(c => new { c.Manufacturer, c.Name, c.Combined });
+
+            //var query = cars.OrderByDescending(c => c.Combined)
+            //    .Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+            //    .OrderByDescending(c => c.Combined)
+            //    .ThenBy(c => c.Name);
+
+            //var first = 
+            //        cars
+            //          .OrderByDescending(c => c.Combined)              
+            //          .OrderByDescending(c => c.Combined)
+            //          .ThenBy(c => c.Name)
+            //          .First(c => c.Manufacturer == "BMW" && c.Year == 2016);
+
+            //var result = cars.Any(c => c.Manufacturer == "Ford");
+            //Console.WriteLine(result);
+            // result = cars.All(c => c.Manufacturer == "Ford");
+            //Console.WriteLine(result);
+
+
         }
 
-        private static List<Car> ProcessFile(string path)
+        private static List<Manufacturer> ProcessManufacturers(string path)
         {
-            return
+            var query =
+                File.ReadAllLines(path)
+                .Where(l => l.Length > 1)
+                .Select(l =>
+                {
+                    var columns = l.Split(',');
+                    return new Manufacturer
+                    {
+                        Name = columns[0],
+                        Headquarters = columns[1],
+                        Year = int.Parse(columns[2])
+                    };
+                });
+            return query.ToList();
+        }
+
+        private static List<Car> ProcessCars(string path)
+        {
+            var quary =
                  File.ReadAllLines(path)
                  .Skip(1)
                  .Where(line => line.Length > 1)
-                 .Select(Car.ParseFromCsv)
-                 .ToList();
+                 .ToCar();
 
+            return quary.ToList();
         }
-
 
     }
 }
