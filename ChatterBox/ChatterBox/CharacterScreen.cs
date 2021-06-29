e4202d216bd8f9a004befdda474c6528fa727487
Ayeager2 <annanyeager@gmail.com>
2021-06-29 15:57:56 -0400

    bootstrap lessons

---
+++ ChatterBox/ChatterBox/CharacterScreen.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatterBox
+{
+    public class CharacterScreen : Controller
+    {
+        // GET: CharacterScreen
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CharacterScreen/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CharacterScreen/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CharacterScreen/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CharacterScreen/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CharacterScreen/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CharacterScreen/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CharacterScreen/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Chatterbox/Chatterbox/wwwroot/js/site.js
+++ Chatterbox/Chatterbox/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+$(document).ready(function () {
+    console.log("ready!");
+});
+
+$(window).on("load", function () {
+    console.log("window loaded");
+});
+
+
+var sidenavbar = $('sidenav');
+
+$('main-navbar').click(function ({
+    console.log("");
+
+$('sidenav').toggle("slow", function () {
+
+});
+});
