1581b170d02d28fe2c21d110689d9d192aea1dc2
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2021-11-03 17:02:30 -0400

    last bit of lessons

--- JSFormWork/Scripts/WebpageSpecific/autocomplete.js
+++ JSFormWork/Scripts/WebpageSpecific/autocomplete.js
@@ -1,12 +1,27 @@
 $(function () {
     $('#type-ahead').typeahead(
         {
-            name: 'football-teams-cached2',
-            prefetch: {
-                url: 'Json/teams.json',
-                ttl: 0
-            }
-        }
-    );
+            template: '<b>{{value}} - </b>Super Bowl Winner in {{year}}',
+            engine:Hogan,
+            // name: 'football-teams-cached2',
+            // prefetch: {
+            //     url: 'Json/teams.json',
+            //     ttl: 0
+            // }
+            local: [
+                {
+                    value: "Giants",
+                    year: 2012,
+                    tokens: ["New York", "Giants"]
+                },
 
+                {
+                    value: "Jets",
+                    year: 1969,
+                    tokens: ["New York", "Jets"]
+                }
+            ]
+
+        });
 });
+            
