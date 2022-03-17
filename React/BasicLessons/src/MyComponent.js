1a3b2ed7fdc0760b11c5eb99140626876641e6cd
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2022-03-17 12:16:55 -0400

    Container components

--- React/BasicLessons/src/MyComponent.js
+++ React/BasicLessons/src/MyComponent.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import MyList from './MyList';
 
-export default class MyComponent extends Component {
-    static defaultProps = {
-        disabled:false,
-        text:'My Button'
-    };
-     render(){
-         const {disabled, text}= this.props;
-
-         return <button disabled={disabled}>{text}</button>;
-     }
+function fetchData(){
+    return new Promise(resolve => {
+        setTimeout(()=> {
+            resolve(['First','Second','Third']);
+        }, 2000);
+    });
 }
+export default class MyContainer extends Component {
+    state = {items: []};
+    componentDidMount(){
+        fetchData().then(items => this.setState({items}));
+    }
+    render() {
+        return <MyList {...this.state} />;
+    }
+}
+
--- React/BasicLessons/src/index.js
+++ React/BasicLessons/src/index.js
@@ -1,28 +1,7 @@
 import React from 'react';
-import {render as renderJSX} from 'react-dom'
-import MyButton from './MyButton';
+import {render } from 'react-dom'
+import MyContainer from './MyComponent';
 
-
-
-function render({first, second}){
-  renderJSX(
-    <main>
-      <MyButton text={first.text} disabled={first.disabled}/>
-      <MyButton text={second.text} disabled={second.disabled}/>
-    </main>,
-    document.getElementById('root')
-  );
-}
-
-render({
-  first: {
-    text: "first button",
-    disabled: false
-  },
-  second: {
-    text:'second button',
-    disabled: true
-  }
-});
+render(<MyContainer/>, document.getElementById('root'));
 
 
