17698d0cf6da42b368098fd3b8fd8b09c095ded0
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2022-03-17 09:20:21 -0400

    Mapping collections to elements

--- React/BasicLessons/src/index.js
+++ React/BasicLessons/src/index.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-const enabled = false;
-const text = 'A Button';
-const placeholder = 'input value...';
-const size = 50;
+const array = ["First", "Second", "Third"];
+
+const object = {
+  first:1,
+  second:2,
+  third: 3
+};
+
 render(
   <section>
-    <button disabled={!enabled}>{text}</button>
-    <input placeholder={placeholder} size={size}></input>
+    <h1>Array</h1>
+    <ul>
+    { array.map(i => (
+        <li key={i}>{i}</li>
+    ))}
+    </ul>
+
+    <h1>Object</h1>
+    <ul>
+      {Object.keys(object).map(i => (
+        <li key={i}>
+          <strong>{i}: </strong>
+          {object[i]}
+        </li>          
+      ))}
+    </ul>
   </section>,
   document.getElementById('root')
 );
