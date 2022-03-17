06e35e6346f18d729be6130fb5a0e6b0e4e44314
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2022-03-17 12:08:00 -0400

    Defaults in functional components

--- React/BasicLessons/src/MyButton.js
+++ React/BasicLessons/src/MyButton.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
-export default class MyButton extends Component {
-    render() {
-        const { disabled, text } = this.props;
-        return <button disabled={disabled}> {text}</button>;
-    }
-}
+const MyButton = ({disabled, text}) => (
+    <button disabled={disabled}>{text}</button>
+);
+
+
+MyButton.defaultProps = {
+    text: 'My Button',
+    disabled:false
+};
+
+export default MyButton;
