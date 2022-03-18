c0cfd2c32a0a5d89f8c651c6ff05370a4d2dd3b6
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2022-03-18 08:53:05 -0400

    Updating state values - hooks

--- React/BasicLessons/src/App.js
+++ React/BasicLessons/src/App.js
@@ -1,14 +1,24 @@
 import React, { Fragment, useState } from 'react';
 
 export default function App() {
-    const [name] = useState('Adamn');
-    const [age] = useState(35);
+    const [name, setName] = useState('Adamn');
+    const [age, setAge] = useState(35);
 
     return (
 
         <Fragment>
-            <p>My name is {name}</p>
-            <p>My age is {age}</p>
+            <section>
+                <input value={name} onChange={e => setName(e.target.value)} />
+                <p>My name is {name}</p>
+            </section>
+            <section>
+                <input
+                    type="number"
+                    value={age}
+                    onChange={e => setAge(e.target.value)}
+                />
+                <p>My age is {age}</p>
+            </section>
         </Fragment>
     );
 }
