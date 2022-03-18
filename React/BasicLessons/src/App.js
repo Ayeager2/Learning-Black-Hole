7c940226f89f802ef90fd148ffbf0aef6ad89c22
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2022-03-18 11:56:22 -0400

    Updating stateful context data

--- React/BasicLessons/src/App.js
+++ React/BasicLessons/src/App.js
@@ -1,17 +1,30 @@
-import React, {Fragment, useState} from 'react';
-import User from './User';
+import React, { useState } from 'react';
+import { StatusProvider } from './StatusContext';
+import { Page1, Page2, Page3, Status } from './Pages';
 
-const ShowHideUser = ({show}) => (show ? <User /> : null);
-
-export default function App(){
-    const [show, setShow] = useState(false);
-
-    return(
-        <Fragment>
-            <button onClick={()=> setShow(!show)}>
-                {show ? 'Hide User' : 'Show User'}
-            </button>
-            <ShowHideUser show={show}/>
-        </Fragment>
-    )
+function ChoosePage({ page }) {
+  const Page = [Page1, Page2, Page3][page];
+  return <Page />;
 }
+
+function App() {
+  const [page, setPage] = useState(0);
+
+  return (
+    <StatusProvider>
+      <button onClick={() => setPage(0)} disabled={page === 0}>
+        Page 1
+      </button>
+      <button onClick={() => setPage(1)} disabled={page === 1}>
+        Page 2
+      </button>
+      <button onClick={() => setPage(2)} disabled={page === 2}>
+        Page 3
+      </button>
+      <ChoosePage page={page} />
+      <Status />
+    </StatusProvider>
+  );
+}
+
+export default App;
