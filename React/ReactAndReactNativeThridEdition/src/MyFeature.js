d90419d6d01bdbe715eeb2b001756ee4f535cab9
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2022-03-19 22:30:14 -0400

    Refactoring class components using Hooks

--- React/ReactAndReactNativeThridEdition/src/MyFeature.js
+++ React/ReactAndReactNativeThridEdition/src/MyFeature.js
@@ -1,6 +1,4 @@
-import React, { Component } from "react";
-import ArticleList from "./ArticleList";
-import AddArticle from "./AddArticle";
+import React, { useState } from "react";
 
 const id = (function*() {
   let i = 1;
@@ -10,102 +8,85 @@ const id = (function*() {
   }
 })();
 
-export default class MyFeature extends Component {
-  state = {
-    articles: [
+export default function MyFeature({ addArticle, articleList }) {
+  const [articles, setArticles] = useState([
+    {
+      id: id.next(),
+      title: "Article 1",
+      summary: "Article 1 Summary",
+      display: "none"
+    },
+    {
+      id: id.next(),
+      title: "Article 2",
+      summary: "Article 2 Summary",
+      display: "none"
+    },
+    {
+      id: id.next(),
+      title: "Article 3",
+      summary: "Article 3 Summary",
+      display: "none"
+    },
+    {
+      id: id.next(),
+      title: "Article 4",
+      summary: "Article 4 Summary",
+      display: "none"
+    }
+  ]);
+
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+
+  function onChangeTitle(e) {
+    setTitle(e.target.value);
+  }
+
+  function onChangeSummary(e) {
+    setSummary(e.target.value);
+  }
+
+  function onClickAdd() {
+    setArticles([
+      ...articles,
       {
         id: id.next(),
-        title: "Article 1",
-        summary: "Article 1 Summary",
-        display: "none"
-      },
-      {
-        id: id.next(),
-        title: "Article 2",
-        summary: "Article 2 Summary",
-        display: "none"
-      },
-      {
-        id: id.next(),
-        title: "Article 3",
-        summary: "Article 3 Summary",
-        display: "none"
-      },
-      {
-        id: id.next(),
-        title: "Article 4",
-        summary: "Article 4 Summary",
+        title: title,
+        summary: summary,
         display: "none"
       }
-    ],
-    title: "",
-    summary: ""
-  };
-
-  onChangeTitle = e => {
-    this.setState({ title: e.target.value });
-  };
-
-  onChangeSummary = e => {
-    this.setState({ summary: e.target.value });
-  };
-
-  onClickAdd = () => {
-    this.setState(state => ({
-      articles: [
-        ...state.articles,
-        {
-          id: id.next(),
-          title: state.title,
-          summary: state.summary,
-          display: "none"
-        }
-      ],
-      title: "",
-      summary: ""
-    }));
-  };
-
-  onClickRemove = id => {
-    this.setState(state => ({
-      ...state,
-      articles: state.articles.filter(article => article.id !== id)
-    }));
-  };
-
-  onClickToggle = id => {
-    this.setState(state => {
-      const articles = [...state.articles];
-      const index = articles.findIndex(article => article.id === id);
-
-      articles[index] = {
-        ...articles[index],
-        display: articles[index].display ? "" : "none"
-      };
-
-      return { ...state, articles };
-    });
-  };
-
-  render() {
-    const { articles, title, summary } = this.state;
-
-    return (
-      <section>
-        <AddArticle
-          name="Articles"
-          title={title}
-          summary={summary}
-          onChangeTitle={this.onChangeTitle}
-          onChangeSummary={this.onChangeSummary}
-          onClickAdd={this.onClickAdd}
-        />
-        <ArticleList
-          articles={articles}
-          onClickToggle={this.onClickToggle}
-          onClickRemove={this.onClickRemove}
-        />
-      </section>
-    );
+    ]);
+    setTitle("");
+    setSummary("");
   }
+
+  function onClickRemove(id) {
+    setArticles(articles.filter(article => article.id !== id));
+  }
+
+  function onClickToggle(id) {
+    const index = articles.findIndex(article => article.id === id);
+    const updatedArticles = [...articles];
+
+    updatedArticles[index] = {
+      ...articles[index],
+      display: articles[index].display ? "" : "none"
+    };
+
+    setArticles(updatedArticles);
+  }
+
+  return (
+    <section>
+      {addArticle({
+        title,
+        summary,
+        onChangeTitle,
+        onChangeSummary,
+        onClickAdd
+      })}
+      {articleList({ articles, onClickToggle, onClickRemove })}
+    </section>
+  );
 }
