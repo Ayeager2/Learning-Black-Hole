3d014f8f03f97b8a1107c689ba278184788f70f8
Ayeager2 <annanyeager@gmail.com>
2022-03-21 10:16:23 -0400

    Requiring specific types

--- React/ReactAndReactNativeThridEdition/src/MyComponent.js
+++ React/ReactAndReactNativeThridEdition/src/MyComponent.js
@@ -1,16 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import MyUser from "./MyUser";
 
-export default function MyComponent({ myHeader, myContent }) {
+export default function MyComponent({ myDate, myCount, myUsers }) {
   return (
     <section>
-      <header>{myHeader}</header>
-      <main>{myContent}</main>
+      <p>{myDate.toLocaleString()}</p>
+      <p>{myCount}</p>
+      <ul>
+        {myUsers.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </section>
   );
 }
 
 MyComponent.propTypes = {
-  myHeader: PropTypes.element.isRequired,
-  myContent: PropTypes.node.isRequired
+  myDate: PropTypes.instanceOf(Date),
+  myCount: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  myUsers: PropTypes.arrayOf(PropTypes.instanceOf(MyUser))
 };
