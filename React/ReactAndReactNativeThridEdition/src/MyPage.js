be2a4daecb331d5d420ece5c364107a377456b28
Ayeager2 <annanyeager@gmail.com>
2022-03-21 14:58:56 -0400

    Simulating latency

--- React/ReactAndReactNativeThridEdition/src/MyPage.js
+++ React/ReactAndReactNativeThridEdition/src/MyPage.js
@@ -1,6 +1,15 @@
 import React, { Fragment, lazy } from "react";
 
-const MyFeature = lazy(() => import("./MyFeature"));
+const MyFeature = lazy(() =>
+  Promise.all([
+    import("./MyFeature"),
+    new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 3000);
+    })
+  ]).then(([m]) => m)
+);
 
 export default function MyPage() {
   return (
