f8c1bf3f5a51df1dd71a4a728916acb312c5e505
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2022-03-23 19:42:21 -0400

    building-responsive-grid-layouts

--- React/ReactAndReactNativeThridEdition/src/App.js
+++ React/ReactAndReactNativeThridEdition/src/App.js
@@ -1,26 +1,76 @@
 import "typeface-roboto";
-import React, { Fragment } from "react";
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
+
+const headerFooterStyle = {
+  padding: 8,
+  textAlign: "center"
+};
+const mainStyle = {
+  padding: 16,
+  textAlign: "center"
+};
+const navStyle = { marginLeft: 5 };
 
 export default function App() {
-  const textStyle = {
-    backgroundColor: "#cfe8fc",
-    margin: 5,
-    textAlign: "center"
-  };
-
   return (
-    <Fragment>
-      <Container maxWidth="sm">
-        <Typography style={textStyle}>sm</Typography>
-      </Container>
-      <Container maxWidth="md">
-        <Typography style={textStyle}>md</Typography>
-      </Container>
-      <Container maxWidth="lg">
-        <Typography style={textStyle}>lg</Typography>
-      </Container>
-    </Fragment>
+    <div style={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper style={headerFooterStyle}>
+            <Typography>Header</Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper>
+            <Grid container spacing={2} direction="column">
+              <Grid item xs={12}>
+                <Typography style={navStyle}>Nav Item 1</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography style={navStyle}>Nav Item 2</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography style={navStyle}>Nav Item 3</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography style={navStyle}>Nav Item 4</Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Paper style={mainStyle}>
+                <Typography>Main Content 1</Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper style={mainStyle}>
+                <Typography>Main Content 2</Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper style={mainStyle}>
+                <Typography>Main Content 3</Typography>
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper style={mainStyle}>
+                <Typography>Main Content 4</Typography>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper style={headerFooterStyle}>
+            <Typography>Footer</Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
