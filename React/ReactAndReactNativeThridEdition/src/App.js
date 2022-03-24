4c3eb564956e8b5b04b8a67d30cd49ef53200a58
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2022-03-23 20:39:47 -0400

    working-with-buttons

--- React/ReactAndReactNativeThridEdition/src/App.js
+++ React/ReactAndReactNativeThridEdition/src/App.js
@@ -1,14 +1,62 @@
 import "typeface-roboto";
-import React from "react";
-import FormGroup from "@material-ui/core/FormGroup";
-import MyTextInput from "./MyTextInput";
-import MySelect from "./MySelect";
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import AndroidIcon from "@material-ui/icons/Android";
+
+const buttonStyle = { margin: 10 };
+
+function toggleColor(setter, value) {
+  setter(value === "default" ? "primary" : "default");
+}
 
 export default function App() {
+  const [contained, setContained] = useState("default");
+  const [text, setText] = useState("default");
+  const [outlined, setOutlined] = useState("default");
+  const [icon, setIcon] = useState("default");
+
   return (
-    <FormGroup style={{ width: 200, margin: 10 }}>
-      <MyTextInput />
-      <MySelect />
-    </FormGroup>
+    <Grid container>
+      <Grid
+        item
+        component={Button}
+        variant="contained"
+        style={buttonStyle}
+        color={contained}
+        onClick={() => toggleColor(setContained, contained)}
+      >
+        Contained
+      </Grid>
+      <Grid
+        item
+        component={Button}
+        style={buttonStyle}
+        color={text}
+        onClick={() => toggleColor(setText, text)}
+      >
+        Text
+      </Grid>
+      <Grid
+        item
+        component={Button}
+        variant="outlined"
+        style={buttonStyle}
+        color={outlined}
+        onClick={() => toggleColor(setOutlined, outlined)}
+      >
+        Outlined
+      </Grid>
+      <Grid
+        item
+        component={IconButton}
+        style={buttonStyle}
+        color={icon}
+        onClick={() => toggleColor(setIcon, icon)}
+      >
+        <AndroidIcon />
+      </Grid>
+    </Grid>
   );
 }
