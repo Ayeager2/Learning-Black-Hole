11af98fbb54ee57733cd57fd7fcfd58ef14a571b
Ayeager2 <67553998+Ayeager2@users.noreply.github.com>
2022-05-18 19:33:56 -0400

    started booking webstie using react

--- React/bookingapp/src/App.js
+++ React/bookingapp/src/App.js
@@ -1,15 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home";
-import List from "./pages/list/List";
-import Hotel from "./pages/hotel/Hotel";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Hotel from "./Pages/Hotel/Hotel";
+import List from "./Pages/List/List";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/hotels" element={<List />} />
-        <Route path="/hotels/:id" element={<Hotel />} />
+        <Route path="/hotel" element={<List />} />
+        <Route path="/hotel/:id" element={<Hotel />} />
+
+
       </Routes>
     </BrowserRouter>
   );
